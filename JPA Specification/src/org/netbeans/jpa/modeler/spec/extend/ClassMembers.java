f03b0742bb910b00466bc4578dbedda9d2786b94
gaurav.gupta <gaurav.gupta.jc@gmail.com>
2016-05-29 23:14:17 +0530

    ClassMember added for toString(), hashCode(), equals(), constructor()

---
+++ JPA Specification/src/org/netbeans/jpa/modeler/spec/extend/ClassMembers.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright [2014] Gaurav Gupta
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.netbeans.jpa.modeler.spec.extend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Gaurav Gupta
+ */
+public class ClassMembers {
+
+   private List<Attribute> attributes;
+
+    public boolean addAttribute(Attribute attribute) {
+        return attributes.add(attribute);
+    }
+    
+    public boolean isExist(Attribute attribute) {
+        return attributes.stream().filter(a -> a == attribute).findAny().isPresent();
+    }
+
+    public boolean removeAttribute(Attribute attribute) {
+        return attributes.remove(attribute);
+    }
+
+    /**
+     * @return the attributes
+     */
+    public List<Attribute> getAttributes() {
+        if(attributes == null){
+            attributes = new ArrayList<>();
+        }
+        return attributes;
+    }
+
+    /**
+     * @param attributes the attributes to set
+     */
+    public void setAttributes(List<Attribute> attributes) {
+        this.attributes = attributes;
+    }
+
+}
