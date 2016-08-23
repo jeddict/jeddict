9633045859fba9ce74d94f0c993ba98163e95a86
jGauravGupta <gaurav.gupta.jc@gmail.com>
2016-08-23 18:39:51 +0530

    License Header added

--- JPA Modeler Core/src/org/netbeans/jpa/modeler/specification/model/util/JPAModelerUtil.java
+++ JPA Modeler Core/src/org/netbeans/jpa/modeler/specification/model/util/JPAModelerUtil.java
@@ -1,5 +1,5 @@
 /**
- * Copyright [2013] Gaurav Gupta
+ * Copyright [2016] Gaurav Gupta
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
