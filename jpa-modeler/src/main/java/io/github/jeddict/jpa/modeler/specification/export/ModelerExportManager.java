cfabdbf37a4d59dea110b36e0259981ba5d44297
Gaurav Gupta <gaurav.gupta.jc@gmail.com>
2019-01-09 12:46:49 +0530

    PPT export incubation module

---
+++ jpa-modeler/src/main/java/io/github/jeddict/jpa/modeler/specification/export/ModelerExportManager.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.jeddict.jpa.modeler.specification.export;
+
+import io.github.jeddict.jpa.modeler.initializer.JPAModelerScene;
+import org.netbeans.modeler.specification.export.IExportManager;
+
+/**
+ *
+ * @author jGauravGupta
+ */
+public interface ModelerExportManager extends IExportManager<JPAModelerScene> {
+    
+}
