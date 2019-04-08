c66a4d13ed7e1b495414d594ee79481f07929f69
Gaurav Gupta <gaurav.gupta.jc@gmail.com>
2019-04-08 22:17:17 +0530

    JNoSQL Generator testcase

--- tests/src/test/java/io/github/jeddict/jnosql/employee/EmployeeModelTest.java
+++ tests/src/test/java/io/github/jeddict/jnosql/employee/EmployeeModelTest.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package io.github.jeddict.jpa.employee;
+package io.github.jeddict.jnosql.employee;
 
 import io.github.jeddict.test.BaseModelTest;
 import org.junit.jupiter.api.*;
