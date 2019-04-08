cdded8f2a9abfc6805553902e01d8de6194c2411
Gaurav Gupta <gaurav.gupta.jc@gmail.com>
2019-04-08 19:52:40 +0530

    @XmlElementWrapper support in XML reverse-engineering parser

--- reverse-engineering/src/main/java/io/github/jeddict/reveng/doc/XmlParser.java
+++ reverse-engineering/src/main/java/io/github/jeddict/reveng/doc/XmlParser.java
@@ -16,6 +16,7 @@
 package io.github.jeddict.reveng.doc;
 
 import static io.github.jeddict.jcode.util.AttributeType.STRING;
+import io.github.jeddict.jcode.util.Inflector;
 import static io.github.jeddict.jcode.util.StringHelper.camelCase;
 import static io.github.jeddict.jcode.util.StringHelper.firstUpper;
 import static io.github.jeddict.jpa.modeler.initializer.JPAModelerUtil.getModelerFileVersion;
@@ -31,6 +32,7 @@ import java.util.function.Consumer;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
@@ -63,12 +65,7 @@ public class XmlParser extends DocParser {
 
             XMLEventReader xmlEventReader = parse(reader);
 
-            JavaClass javaClass;
-            if (jpaSupport) {
-                javaClass = generateEntity(entityMappings, xmlEventReader);
-            } else {
-                javaClass = generateClass(entityMappings, xmlEventReader);
-            }
+            JavaClass javaClass = generateClass(entityMappings, xmlEventReader, jpaSupport);
             javaClass.setXmlRootElement(jaxbSupport);
             entityMappings.setJaxbSupport(jaxbSupport);
 
@@ -84,133 +81,104 @@ public class XmlParser extends DocParser {
         return xmlEventReader;
     }
 
-    private JavaClass generateClass(EntityMappings entityMappings, XMLEventReader xmlEventReader) throws XMLStreamException {
-
-        BeanClass rootClass = null;
-
-        Stack<BeanClass> beanClasses = new Stack();
+    private JavaClass generateClass(EntityMappings entityMappings, XMLEventReader xmlEventReader, boolean entity) throws XMLStreamException {
+        Inflector inflector = Inflector.getInstance();
+        JavaClass rootClass = null;
+        Stack<JavaClass> classes = new Stack();
+        Stack<String> wrappers = new Stack(); // JAXB @XmlElementWrapper
         while (xmlEventReader.hasNext()) {
-            boolean clazz = false;
+            boolean isClass = false;
+            boolean isWrapper = false;
             XMLEvent xmlEvent = xmlEventReader.nextEvent();
             if (xmlEvent.isStartElement()) {
                 StartElement startElement = xmlEvent.asStartElement();
                 String key = startElement.getName().getLocalPart();
                 XMLEvent peekEvent = xmlEventReader.peek();
                 if (startElement.getAttributes().hasNext()) {
-                    clazz = true;
+                    isClass = true;
                 } else if (peekEvent.isCharacters()) {
                     if (peekEvent.asCharacters().getData().trim().isEmpty()) {
                         xmlEventReader.next();
                         if (xmlEventReader.hasNext()) {
                             peekEvent = xmlEventReader.peek();
                             if (peekEvent.isStartElement()) {
-                                clazz = true;
+                                StartElement peekStartElement = peekEvent.asStartElement();
+                                String peekKey = peekStartElement.getName().getLocalPart();
+                                if (inflector.singularize(key).equalsIgnoreCase(peekKey)
+                                        && !key.equalsIgnoreCase(peekKey)) {
+                                    isWrapper = true;
+                                } else {
+                                    isClass = true;
+                                }
                             }
                         }
                     } else {
-                        clazz = false;
+                        isClass = false;
                     }
                 }
-                BeanClass prevBeanClass = beanClasses.isEmpty() ? null : beanClasses.peek();
-                if (clazz) {
+                JavaClass prevClass = classes.isEmpty() ? null : classes.peek();
+                if (isClass) {
                     String className = firstUpper(camelCase(key));
-                    BeanClass beanClass = createBeanClass(entityMappings, key);
+                    JavaClass clazz = entity ? createEntity(entityMappings, key) : createBeanClass(entityMappings, key);
                     reporter.accept(getMessage(DocWizardDescriptor.class, "MSG_Progress_Class_Parsing", className));
                     if (rootClass == null) {
-                        rootClass = beanClass;
+                        rootClass = clazz;
                     }
-                    beanClasses.push(beanClass);
+                    classes.push(clazz);
 
                     if (startElement.getAttributes().hasNext()) {
                         Iterator attributesItr = startElement.getAttributes();
                         while (attributesItr.hasNext()) {
-                            javax.xml.stream.events.Attribute attribute = (javax.xml.stream.events.Attribute) attributesItr.next();
-                            createBeanAttribute(beanClass, STRING, attribute.getName().getLocalPart());
+                            Attribute attribute = (Attribute) attributesItr.next();
+                            if (entity) {
+                                createBasicAttribute((Entity) clazz, STRING, attribute.getName().getLocalPart());
+                            } else {
+                                createBeanAttribute((BeanClass) clazz, STRING, attribute.getName().getLocalPart());
+                            }
                         }
                     }
 
-                    if (prevBeanClass != null) {
-                        createOneToManyAssociation(prevBeanClass, beanClass, key);
+                    if (prevClass != null) {
+                        if (!wrappers.isEmpty()
+                                && inflector.singularize(wrappers.peek()).equalsIgnoreCase(key)) {
+                            if (entity) {
+                                createOneToMany((Entity) prevClass, (Entity) clazz, wrappers.peek());
+                            } else {
+                                createOneToManyAssociation((BeanClass) prevClass, (BeanClass) clazz, wrappers.peek());
+                            }
+                        } else {
+                            if (entity) {
+                                createOneToMany((Entity) prevClass, (Entity) clazz, key);
+                            } else {
+                                createOneToManyAssociation((BeanClass) prevClass, (BeanClass) clazz, key);
+                            }
+                        }
                     }
+                } else if (isWrapper) {
+                    wrappers.push(key);
                 } else {
-                    createBeanAttribute(prevBeanClass, STRING, key);
+                    if(entity){
+                        createBasicAttribute((Entity) prevClass, STRING, key);
+                    } else {
+                        createBeanAttribute((BeanClass) prevClass, STRING, key);
+                    }
                 }
             }
             if (xmlEvent.isEndElement()) {
                 EndElement endElement = xmlEvent.asEndElement();
                 String key = endElement.getName().getLocalPart();
-                BeanClass prevBeanClass = beanClasses.isEmpty() ? null : beanClasses.peek();
-                if (prevBeanClass != null && prevBeanClass.getName().equals(firstUpper(camelCase(key)))) {
-                    beanClasses.pop();
+                JavaClass prevClass = classes.isEmpty() ? null : classes.peek();
+                if (prevClass != null && prevClass.getName().equals(firstUpper(camelCase(key)))) {
+                    classes.pop();
+                }
+                String peekWrapper = wrappers.isEmpty() ? null : wrappers.peek();
+                if (peekWrapper != null && peekWrapper.equalsIgnoreCase(key)) {
+                    wrappers.pop();
                 }
             }
         }
 
         return rootClass;
-    }
-
-    private Entity generateEntity(EntityMappings entityMappings, XMLEventReader xmlEventReader) throws XMLStreamException {
-
-        Entity rootEntity = null;
-        Stack<Entity> entities = new Stack();
-        while (xmlEventReader.hasNext()) {
-            boolean clazz = false;
-            XMLEvent xmlEvent = xmlEventReader.nextEvent();
-            if (xmlEvent.isStartElement()) {
-                StartElement startElement = xmlEvent.asStartElement();
-                String key = startElement.getName().getLocalPart();
-                XMLEvent peekEvent = xmlEventReader.peek();
-                if (startElement.getAttributes().hasNext()) {
-                    clazz = true;
-                } else if (peekEvent.isCharacters()) {
-                    if (peekEvent.asCharacters().getData().trim().isEmpty()) {
-                        xmlEventReader.next();
-                        if (xmlEventReader.hasNext()) {
-                            peekEvent = xmlEventReader.peek();
-                            if (peekEvent.isStartElement()) {
-                                clazz = true;
-                            }
-                        }
-                    } else {
-                        clazz = false;
-                    }
-                }
-                Entity prevEntity = entities.isEmpty() ? null : entities.peek();
-                if (clazz) {
-                    String className = firstUpper(camelCase(key));
-                    Entity entity = createEntity(entityMappings, key);
-                    reporter.accept(getMessage(DocWizardDescriptor.class, "MSG_Progress_Class_Parsing", className));
-                    if (rootEntity == null) {
-                        rootEntity = entity;
-                    }
-                    entities.push(entity);
-
-                    if (startElement.getAttributes().hasNext()) {
-                        Iterator attributesItr = startElement.getAttributes();
-                        while (attributesItr.hasNext()) {
-                            javax.xml.stream.events.Attribute attribute = (javax.xml.stream.events.Attribute) attributesItr.next();
-                            createBasicAttribute(entity, STRING, attribute.getName().getLocalPart());
-                        }
-                    }
-
-                    if (prevEntity != null) {
-                        createOneToMany(prevEntity, entity, key);
-                    }
-                } else {
-                    createBasicAttribute(prevEntity, STRING, key);
-                }
-            }
-            if (xmlEvent.isEndElement()) {
-                EndElement endElement = xmlEvent.asEndElement();
-                String key = endElement.getName().getLocalPart();
-                Entity prevEntity = entities.isEmpty() ? null : entities.peek();
-                if (prevEntity != null && prevEntity.getName().equals(firstUpper(camelCase(key)))) {
-                    entities.pop();
-                }
-            }
-        }
-
-        return rootEntity;
     }
 
 }
